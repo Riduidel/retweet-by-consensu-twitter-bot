34e5092fc39948fe50a62f3c149f47940a5a5800
Nicolas Delsaux <nicolas.delsaux@gmx.fr>
2020-06-04 15:09:48 +0200

    Tests have no more any meaning

--- bot/src/test/java/org/ndx/retweet/by/consensus/bot/NativeTwitterWebHookReceiverIT.java
+++
@@ -1,9 +0,0 @@
-package org.ndx.retweet.by.consensus.bot;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeTwitterWebHookReceiverIT extends TwitterWebHookReceiverTest {
-
-    // Execute the same tests but in native mode.
-}
